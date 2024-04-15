82a5a75154a3920fe6b30ec5f669cea6a86aed41
Abhishek Shankar Choudhary <theabhishek.srm@gmail.com>
2024-04-15 20:58:05 +0530

    let vs const

---
+++ letVsConst.js
@@ -0,0 +1,15 @@
+// Number, boolean, string,
+// const a = 10 
+
+// a = 10 // Error: Assignment to constant variable.
+
+// console.log(a) // 100
+
+const arr = [10, 20, 30, 40, 50]  // 1000th memory location
+
+// arr[0] = 100
+// arr[1] = 1000
+
+// arr = [1,2,3,45]  // 2000th memory location
+
+console.log(arr) // [100, 20, 30, 40, 50]
---
+++ questions.js
@@ -0,0 +1,9 @@
+
+// let arr1 = [10,20,30,40,50]
+// arr1 = []
+// console.log(arr1)
+
+
+// const arr2 = [10,20,30,40,50,60]
+// arr2.length = 0
+// console.log(arr2)
